ced7cd8f4ae4688aabc74850957f9f8744a4dae5
wang_125309 <wang_125309@foxmail.com>
2015-04-29 01:26:12 +0800

    push

--- assets/js/.js
+++ assets/js/.js
@@ -1,29 +1,103 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 $ = require("../../../bower_components/jquery/dist/jquery.js");
-
+require("./lib/logout.js");
 $(function(){
-    $.get("/benz/backend/getProblemId/",function(d){
-        console.log(d.problemId);
-        if(d.problemId != $("#id").data("id")) {
-            location.href = "/benz/backend/problem/?id="+d.problemId; 
-        }
-        else {
-            setInterval(function(){
-                $(".s"+$(".num").html()).css({
-                    "background":"#1f1f1f"
-                });
-                $(".num").html($(".num").html()-1);
-                if($(".num").html() <= '0') {
-                    $.get("/benz/backend/setProblemId/?id="+parseInt($("#id").data("id")+1),function(){
-                        location.href = "/benz/backend/problem/?id="+parseInt($("#id").data("id")+1);
-                    });
+    var edit_id = 0;
+    var saved = 0;
+    var del_id = 0;
+    $(".del").on("click",function(){
+        del_id = $(this).data("id"); 
+        $(".name").text(del_id);
+        $("#del-menu").fadeIn();
+    });
+    $(".delete").on("click",function(){
+
+        $.get("/benz/backend/deleteUser/?id="+del_id,function(d){
+            if(d.status == 'success') {
+                location.href = location.href;
+            }
+            else {
+                alert(d.reason);
+            }
+        });
+    });
+    $(".opt").on("click",function(){
+        saved = 0;
+        edit_id = $(this).data("id");
+        $("#edit-user").fadeIn();
+        $.get("/benz/backend/getUserMessage/?id="+$(this).data("id"),function(d){
+            $("#username").val(d.data.username);
+            $("#password").val(d.data.password);
+            if(d.data.type == 1) {
+                $("#normal").prop({"checked":"checked"});
+            }
+            else {
+                $("#controller").prop({"checked":"checked"});
+            }
+        });
+    });
+    $("#addUser").on("click",function(){
+        saved = 0;
+        edit_id = 0
+        $("#edit-user").fadeIn();
+        $("#username").val("");
+        $("#password").val("");
+        $("#normal").prop("checked","");
+        $("#controller").prop("checked","");
+    });
+
+    $(".save").on("click",function(){
+        if(!saved) {
+            first = $("#normal").prop("checked");
+            if(first == true) {
+                pri = 1;
+            }
+            else {
+                pri = 2;
+            }
+            $.post("/benz/backend/saveUser/?id="+edit_id,{
+                "username":$("#username").val(),
+                "password":$("#password").val(),
+                "pri":pri
+            },function(d){
+                if(d.status == "success") {
+                    location.href = location.href;
                 }
-            },1000);
-         }
-    }); 
+                else {
+                    alert(d.reason);
+                }
+            });
+            saved = 1;
+        }
+        $("#edit-user").fadeOut();
+    });
+    $(".cancel").on("click",function(){
+        $(".menu").fadeOut();
+    });
+    
 });
 
-},{"../../../bower_components/jquery/dist/jquery.js":2}],2:[function(require,module,exports){
+},{"../../../bower_components/jquery/dist/jquery.js":3,"./lib/logout.js":2}],2:[function(require,module,exports){
+$(function(){
+    $("#taskList").on("click",function(){
+        location.href = "/benz/backend/taskList/";
+    });
+    $("#userList").on("click",function(){
+        location.href = "/benz/backend/userList/";
+    });
+    $("#quit").on("click",function() {
+        $.get("/benz/backend/quitAction/",function(d){
+            if(d.status == "success") {
+                location.href = "/benz/backend/login/";
+            }
+            else {
+                alert("登出错误");
+            }
+        });
+    });
+});
+
+},{}],3:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v2.1.3
  * http://jquery.com/
@@ -9210,87 +9284,6 @@ jQuery.noConflict = function( deep ) {
 	}
 
 	if ( deep && window.jQuery === jQuery ) {
-		window.jQuery = _jQuery;
-	}
-
-	return jQuery;
-};
-
-// Expose jQuery and $ identifiers, even in AMD
-// (#7102#comment:10, https://github.com/jquery/jquery/pull/557)
-// and CommonJS for browser emulators (#13566)
-if ( typeof noGlobal === strundefined ) {
-	window.jQuery = window.$ = jQuery;
-}
-
-
-
-
-return jQuery;
-
-}));
-
-},{}]},{},[1])ery.fn.andSelf = jQuery.fn.addBack;
-
-
-
-
-// Register as a named AMD module, since jQuery can be concatenated with other
-// files that may use define, but not via a proper concatenation script that
-// understands anonymous AMD modules. A named AMD is safest and most robust
-// way to register. Lowercase jquery is used because AMD module names are
-// derived from file names, and jQuery is normally delivered in a lowercase
-// file name. Do this after creating the global so that if an AMD module wants
-// to call noConflict to hide this version of jQuery, it will work.
-
-// Note that for maximum portability, libraries that are not jQuery should
-// declare themselves as anonymous modules, and avoid setting a global if an
-// AMD loader is present. jQuery is a special case. For more information, see
-// https://github.com/jrburke/requirejs/wiki/Updating-existing-libraries#wiki-anon
-
-if ( typeof define === "function" && define.amd ) {
-	define( "jquery", [], function() {
-		return jQuery;
-	});
-}
-
-
-
-
-var
-	// Map over jQuery in case of overwrite
-	_jQuery = window.jQuery,
-
-	// Map over the $ in case of overwrite
-	_$ = window.$;
-
-jQuery.noConflict = function( deep ) {
-	if ( window.$ === jQuery ) {
-		window.$ = _$;
-	}
-
-	if ( deep && window.jQuery === jQuery ) {
-		window.jQuery = _jQuery;
-	}
-
-	return jQuery;
-};
-
-// Expose jQuery and $ identifiers, even in AMD
-// (#7102#comment:10, https://github.com/jquery/jquery/pull/557)
-// and CommonJS for browser emulators (#13566)
-if ( typeof noGlobal === strundefined ) {
-	window.jQuery = window.$ = jQuery;
-}
-
-
-
-
-return jQuery;
-
-}));
-
-},{}]},{},[1]) deep && window.jQuery === jQuery ) {
 		window.jQuery = _jQuery;
 	}
 
